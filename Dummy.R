11f1e61badbae75d1db5996b396d5e4b633e3ade
wutingfan <wutingfan2017@gmail.com>
2019-06-08 17:04:48 +0800

    Add files via upload

---
+++ Dummy.R
@@ -0,0 +1,14 @@
+
+
+data_dummy <- function(data){
+  # data need to change
+
+  library(caret)
+  dmy = dummyVars(formula = " ~ .", data = data)
+  data_trsf = data.frame(predict(dmy, newdata = data))
+
+  out = list(data_trsf = data_trsf)
+
+  return(out)
+
+}
