a541b76ff1a780806870e81e84411b2434d4c46b
wutingfan <wutingfan2017@gmail.com>
2019-06-25 05:36:16 +0800

    Add files via upload

---
+++ ALBERT/R/radscore.R
@@ -0,0 +1,23 @@
+# Radiomics score
+radscore <- function(model,train_set,test_set){
+  
+  # model : logistic features
+  # train_set:  features
+  Data  <- data.frame(rbind(train_set,test_set))
+  xn <- dim(Data)[1]      # row
+  yn <- dim(Data)[2]      # column
+  coefPara       <- coef(model, s="lambda.min")
+  beta           <- as.matrix(  coefPara[ which(coefPara !=0), ] )   # get beta = Coefficients
+  betai_Matrix   <- as.matrix( beta[-1] )      # get betai
+  beta0_Matrix   <- matrix(beta[1], xn, 1 )    # get beta0
+  Rad_Matrix     <- as.matrix( Data )          # get X
+  Radcore_Matrix <- Rad_Matrix %*% betai_Matrix + beta0_Matrix  # get Radscore
+  radscore_all   <- as.numeric(Radcore_Matrix)
+  Radscore_train <- radscore_all[1:dim(train_set)[1]]    # train rads
+  Radscore_test  <- radscore_all[c( (dim(train_set)[1]+1) :xn)] # test rads
+  
+  return(list(Radscore_train = Radscore_train,
+              Radscore_test  = Radscore_test))
+  
+  
+}
